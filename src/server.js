bf43a90c9ec7c145a77d095aa6be405c24266e30
Marina <marina.miram@gmail.com>
2026-06-12 21:12:09 +0300

    .

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { connectMongoDB } from './db/connectMongoDB.js';
-
+import { errors } from "celebrate";
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { logger } from './middleware/logger.js';
@@ -35,6 +35,8 @@ app.use((req, res, next) => {
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
+
+app.use(errors());
 
 app.use(errorHandler);
 
